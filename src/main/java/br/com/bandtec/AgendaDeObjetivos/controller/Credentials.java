f04c419206a4b1347d27e10af7fe440adb310b42
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-08-19 19:35:58 -0300

    controller method post login

--- src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/Credentials.java
+++ src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/Credentials.java
@@ -2,8 +2,20 @@ package br.com.bandtec.AgendaDeObjetivos.controller;
 
 public class Credentials {
 
-	public Credentials(String string, String string2) {
+	private String login;
+	private String password;
+
+	public Credentials(String login, String password) {
+		this.login = login;
+		this.password = password;
 		// TODO Auto-generated constructor stub
+	}
+
+	public String getLogin() {
+		return login;
+	}
+	public String getPassword() {
+		return password;
 	}
 
 }
--- src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/LoginController.java
+++ src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/LoginController.java
@@ -1,13 +1,20 @@
 package br.com.bandtec.AgendaDeObjetivos.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class LoginController {
 
-	public ResponseEntity<String> validateLogin(Credentials credentials) {
-		return ResponseEntity.ok("Success");
-		// TODO Auto-generated method stub
-		
+	@PostMapping("/login")
+	public ResponseEntity<String> validateLogin(@RequestBody Credentials credentials) {
+		if (credentials.getPassword().equals(credentials.getLogin())) {
+			return ResponseEntity.ok("Success");
+		}
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Failed");
 	}
-	
+
 }
