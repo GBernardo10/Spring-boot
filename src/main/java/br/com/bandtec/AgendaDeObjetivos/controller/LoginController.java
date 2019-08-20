ef9db9e33c09920a619421cfa81690e06457e62b
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-08-19 21:05:01 -0300

    adiciona controller para buscar usuarios pelo nome ou pela idade

--- src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/LoginController.java
+++ src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/LoginController.java
@@ -2,13 +2,13 @@ package br.com.bandtec.AgendaDeObjetivos.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
-
 	@PostMapping("/login")
 	public ResponseEntity<String> validateLogin(@RequestBody Credentials credentials) {
 		if (credentials.getPassword().equals(credentials.getLogin())) {
@@ -16,5 +16,4 @@ public class LoginController {
 		}
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Failed");
 	}
-
 }
