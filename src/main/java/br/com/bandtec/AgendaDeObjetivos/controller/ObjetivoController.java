8ab6aea7903e20629e97bc86b1de3e852e22cb5d
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-09-09 19:05:33 -0300

    adiciona o controller e o model e o repositorio para a funcionalidade de cadastrar objetivo e buscar pela data

---
+++ src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/ObjetivoController.java
@@ -0,0 +1,36 @@
+package br.com.bandtec.AgendaDeObjetivos.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ObjetivoController {
+
+	private TodosObjetivos todosObjetivos;
+
+	@Autowired
+	public ObjetivoController(TodosObjetivos todosObjetivos) {
+		this.todosObjetivos = todosObjetivos;
+	}
+
+	@PostMapping("/objetivos")
+	public ResponseEntity<String> cadastraObjetivo(@RequestBody Objetivo objetivo) {
+		todosObjetivos.salvar(objetivo);
+		return ResponseEntity.ok("Sucesso");
+	}
+
+	@GetMapping("/objetivos/data/{data}")
+	public List<Objetivo> buscar(@PathVariable("data") String dataString) {
+		LocalDate data = LocalDate.parse(dataString);
+		return todosObjetivos.ate(data);
+	}
+
+}
