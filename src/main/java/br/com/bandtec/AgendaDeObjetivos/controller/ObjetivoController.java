acf774c3af2c7694096ad43e30763dbc683a886a
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-09-09 20:55:40 -0300

    adiciona a interface repository

--- src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/ObjetivoController.java
+++ src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/ObjetivoController.java
@@ -2,7 +2,6 @@ package br.com.bandtec.AgendaDeObjetivos.controller;
 
 import java.time.LocalDate;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/TodosObjetivos.java
+++ src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/TodosObjetivos.java
@@ -1,29 +1,17 @@
 package br.com.bandtec.AgendaDeObjetivos.controller;
 
-import static java.util.stream.Collectors.toList;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TodosObjetivos {
+public interface TodosObjetivos extends  JpaRepository<Objetivo, Integer>{
 
-	private final List<Objetivo> objetivos;
+	public void salvar(Objetivo objetivo);
 
-	public TodosObjetivos() {
-		this.objetivos = new ArrayList<>();
-	}
-
-	public void salvar(Objetivo objetivo) {
-		this.objetivos.add(objetivo);
-		System.out.println("Objetivo " + objetivo + " cadastrado com sucesso");
-	}
-
-	public List<Objetivo> ate(LocalDate data) {
-		return objetivos.stream().filter(o -> o.ate(data)).collect(toList());
-	}
+	public List<Objetivo> ate(LocalDate data);
 
 }
